af7c23d6cf1f78b6a583f7fb3be0177e129c675f
ogyreck <andrey010804@gmail.com>
2024-08-11 19:28:35 +0400

    Создание модели Pet

---
+++ backend/src/PetHelp.Domain/Models/Pet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelp.Domain.Models
+{
+	public class Pet
+	{
+
+		private readonly List<Requisites> _requisitesForHelp = [];
+
+		public Guid Id { get; set; }
+		public string Name { get; set; } = default!;
+
+		public Species? Species { get; set; }
+
+		public string Description { get; set; } = default!;
+
+		public Breed? Breed { get; set; }
+
+		public string Color { get; set; } = default!;
+
+		public string HealthInfo { get; set; } = default!;
+
+		public string Adress { get; set; } = default!;
+
+		public double Weight { get; set; }
+
+		public double Height { get; set; }
+
+		public string OwnerPhone { get; set; } = default!;
+
+		public bool IsCostrated { get; set; }
+
+		public DateOnly DateOfBirth { get; set; }
+
+		public bool IsVaccinated { get; set; }
+
+		public HelpStatus HelpStatus { get; set; }
+
+		public IReadOnlyList<Requisites> RequisitesForHelp => _requisitesForHelp;
+
+		public DateTime DateOfCreate { get; set; }
+
+	}
+}
---
+++ backend/src/PetHelp.Domain/Models/Species.cs
@@ -0,0 +1,14 @@
+﻿namespace PetHelp.Domain.Models
+{
+	public class Species
+	{
+		private readonly List<Breed> _breeds = [];
+
+		public Guid Id { get; private set; }
+
+		public string Name { get; private set; } = default!;
+
+		public IReadOnlyList<Breed> Breeds => _breeds;
+
+	}
+}
